91794f04ee2a8950f9b45d626b37f2daf77c3b5a
lidemar.santos <lildemar.santos@totvs.com.br>
2019-06-21 13:31:32 -0300

    Criando o servidor de autorização

---
+++ sales-engine-auth-server/src/main/java/br/com/lms/salesengine/authserver/SalesEngineAuthServerApplication.java
@@ -0,0 +1,13 @@
+package br.com.lms.salesengine.authserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalesEngineAuthServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalesEngineAuthServerApplication.class, args);
+	}
+
+}
