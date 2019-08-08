b8614f6e10b1e8da9d653e4906ac0eb94b4f077c
lidemar.santos <lildemar.santos@totvs.com.br>
2019-08-08 15:55:42 -0300

    Adicionando estutura do Payments Service

---
+++ sales-engine-payments-service/src/test/java/br/com/lms/paymentsservice/SalesEnginePaymentsServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package br.com.lms.paymentsservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SalesEnginePaymentsServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
