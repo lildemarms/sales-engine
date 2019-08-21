9fedc1fc5bf76e620e2023566353741cea6de2a0
lidemar.santos <lildemar.santos@totvs.com.br>
2019-08-21 12:40:49 -0300

    Testes na implementação do admin server

--- salesengine-admin-server/src/main/java/br/com/lms/salesengine/adminserver/SalesEngineAdminServerApplication.java
+++ salesengine-admin-server/src/main/java/br/com/lms/salesengine/adminserver/SalesEngineAdminServerApplication.java
@@ -1,18 +1,12 @@
 package br.com.lms.salesengine.adminserver;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 import de.codecentric.boot.admin.server.config.EnableAdminServer;
 
 @SpringBootApplication
-@EnableAutoConfiguration
-@EnableDiscoveryClient
 @EnableAdminServer
-@EnableEurekaClient
 public class SalesEngineAdminServerApplication {
 
 	public static void main(String[] args) {
